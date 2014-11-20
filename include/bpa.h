012a5e24852975625902b46566dce03dc81a17fb
jefg89 <jefg89@gmail.com>
2014-11-20 08:39:42 -0600

    bpa implementation

--- include/bpa.h
+++ include/bpa.h
@@ -39,5 +39,60 @@ void BPA (float **In,float *d_pk, float *b); //
 
 
 
+//Ending head-------------------------------------------
+
+
+
+
+
+//------------Backpropagation algorithm-------------------------
+void BPA (float **In,float *d_pk, float * b) {
+int i = 0;
+int j=0;
+int iter=0;
+
+//------------ Initial random weights generation//
+
+srand ((unsigned) (time(0)));
+for (i=0;i<N_hidden;i++){
+	w_kj [i] =(float) (rand()) / (float) (RAND_MAX);
+	for (j=0;j<N_in;j++) {
+        w_ji [i][j]=(float) (rand()) / (float) (RAND_MAX);
+		}
+	}
+
+
+
+
+
+*b=-1.72; //adjust bias manually.
+
+//------------ End of random weigths generation
+
+//----------Back_propagation-----------------------//
+for (iter;iter<N_iter;iter++) {
+	int pos =0;
+	for (pos;pos<N_dat;pos++){
+		ANN (In[pos], w_ji, w_kj,b,y_pj,y_pk);//ANN run
+		//printf("salida %d = %f \n",pos,*y_pk);
+ 
+		//output delta computing
+		delta_pk=(d_pk[pos]-*y_pk); 
+		//hidden delta computing
+		 for (i=0;i<N_hidden;i++) {
+			delta_pj[i]= y_pj[i] *( 1.0 - y_pj[i])* delta_pk * w_kj[i];
+			w_kj[i] =  w_kj[i] + eta * delta_pk * y_pj[i];  //output weights adjustment
+			for (j=0;j<N_in;j++) {
+				w_ji[i][j] =  w_ji[i][j] + eta * delta_pj[i] * In[pos][j]; // hidden weight adjustment
+			} 
+			
+			}
+			*b=*b + eta * delta_pk ;
+		}
+	}
+
+}
+
+
 #endif
 
