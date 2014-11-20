5c70964de2059c7218732298ebe475df0c919cc2
jefg89 <jefg89@gmail.com>
2014-11-20 08:35:28 -0600

    bpa header

---
+++ include/bpa.h
@@ -0,0 +1,43 @@
+#ifndef _ANN_H
+#define _ANN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int N_hidden=0; /*number of hidden neurons in layer*/
+int N_in=0;  /*number of input variables*/
+int N_iter=0; /*number of training iterations*/
+int N_dat=0;  /*number of training data*/
+
+/*activation function mode*/
+int actFunc=0; 
+/*0 = logsig , 1= linear  /*
+/*------------------------*/
+
+
+
+float eta=0.0; /*learning speed*/
+float *b;
+
+float * x_pi;
+float *y_pj; //hidden out
+float *y_pk; // output out
+float delta_pk; //output layer delta
+float * delta_pj; //hidden layer delta
+float **w_ji;
+float *w_kj;
+
+
+/*N input - single output ANN*/ //replace with hardware
+void ANN (float * In, float ** w_ji, float  * w_kj, float *b, float * y_pj,float * y_pk);
+
+
+/*one dimensional input - backpropagation algorithm*/
+void BPA (float **In,float *d_pk, float *b); //
+
+
+
+#endif
+
